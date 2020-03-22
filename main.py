775a149e5e387c4b27a1a5096a1b1bb5ba64a56a
David Drobner <david@drobner.ca>
2020-03-21 22:05:34 -0400

    Writes data to csv file

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pyzbar.pyzbar as pyzbar
 import cv2
+import csv
+import pathlib
 
 camera = cv2.VideoCapture(-1)
+
+"""
+if not pathlib.Path('data.csv').is_file():
+    f = open('data.csv', 'a')
+    f.write("Name,Match Number,Team Number,Alliance,Preloaded Cells,High Port Auto,Low Port Auto,Missed Auto,High Port Teleop,Low Port Teleop,Missed Teleop,Robot Attitude,Colour Wheel Done,Colour Wheel Landed on Colour,Colour Wheel Was Rotated,Did Climb,Balanced,Number of Climbs,Notes,Confidence")
+    f.close()
+"""
 
 while True:
     ret, frame = camera.read()
@@ -11,7 +20,9 @@ while True:
     decodedBarcode = pyzbar.decode(frame)
     if decodedBarcode:
         scoutingData = decodedBarcode[0].data.decode('utf-8').split(", ")
-        print(scoutingData)
+        with open('data.csv', 'a') as out_file:
+            wr = csv.writer(out_file)
+            wr.writerow(scoutingData)
         break
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
