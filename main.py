1e1f2525f4dfe1216b3b793ff98182d652847673
David Drobner <david@drobner.ca>
2020-03-21 21:05:39 -0400

    Made webcam actually work

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pyzbar.pyzbar as pyzbar
 import cv2
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(-1)
 
 while True:
     ret, frame = camera.read()
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', grayscale)
 
-    decodedBarcode = pyzbar.decode(grayscale)
+    decodedBarcode = pyzbar.decode(frame)
     if decodedBarcode:
-        print(decodedBarcode.data)
+        print(decodedBarcode[0].data)
         break
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
