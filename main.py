e8419de15432b153700d8d08586dd341da7c844a
David Drobner <david@drobner.ca>
2020-03-22 21:49:34 -0400

    Better GUI

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
 from Scan import Scan
+from PIL import ImageTk, Image
 
 class Odin(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.geometry("600x400")
+        self.title("Odin")
         self.scan = Scan("data.csv")
 
-        self.frame = ttk.Frame(self)
-        self.frame.pack()
+        self.columnconfigure(0, weight=1)
 
-        self.entry = ttk.Entry(self.frame)
+        self.img = ImageTk.PhotoImage(Image.open("logo_image.png"))
+        self.panel = tk.Label(self, image=self.img)
+        self.panel.grid(row=0, column=0)
 
-        self.scan_button = ttk.Button(self.frame, text="Scan Code", command=self.scan.scan)
-        self.scan_button.pack()
+        self.scan_button = ttk.Button(text="Scan Code", command=self.scan.scan)
+        self.scan_button.grid(row=1, column=0)
 
         self.style = ttk.Style()
         self.style.theme_use("clam")
