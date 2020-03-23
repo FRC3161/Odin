5d3ba0a2f3da3b849c25f938a8c6afdff992e9c6
David Drobner <david@drobner.ca>
2020-03-22 20:46:38 -0400

    Restructured project

---
+++ QR.py
@@ -0,0 +1,35 @@
+import pyzbar.pyzbar as pyzbar
+import cv2
+
+# TODO probably should refactor at some point
+
+class QR:
+    def __init__(self):
+        self.ret = None
+        self.frame = None
+        self.decodedData = None
+        self.grayscale = None
+        self.camera = cv2.VideoCapture(0)
+
+    def read_camera(self):
+        self.ret, self.frame = self.camera.read()
+        self.grayscale = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', self.grayscale)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            return
+
+        return self.grayscale
+
+    def decode(self, data):
+        self.decodedData = pyzbar.decode(data)
+        if(self.decodedData):
+            return self.decodedData[0].data.decode('utf-8').split(", ")
+        else:
+            return None
+
+    def destroy_windows(self):
+        cv2.destroyAllWindows()
+
+    def release_camera(self):
+        self.camera.release()
--- main.py
+++ main.py
@@ -1,32 +1,17 @@
-import pyzbar.pyzbar as pyzbar
-import cv2
-import csv
-import pathlib
+from QR import QR
+from CSVHandler import CSVHandler
 
-camera = cv2.VideoCapture(-1)
+csvfile = open('data.csv', 'a')
 
-"""
-if not pathlib.Path('data.csv').is_file():
-    f = open('data.csv', 'a')
-    f.write("Name,Match Number,Team Number,Alliance,Preloaded Cells,High Port Auto,Low Port Auto,Missed Auto,High Port Teleop,Low Port Teleop,Missed Teleop,Robot Attitude,Colour Wheel Done,Colour Wheel Landed on Colour,Colour Wheel Was Rotated,Did Climb,Balanced,Number of Climbs,Notes,Confidence")
-    f.close()
-"""
+qr_reader = QR()
+csv_writer = CSVHandler(csvfile)
 
 while True:
-    ret, frame = camera.read()
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', grayscale)
-
-    decodedBarcode = pyzbar.decode(frame)
-    if decodedBarcode:
-        scoutingData = decodedBarcode[0].data.decode('utf-8').split(", ")
-        with open('data.csv', 'a') as out_file:
-            wr = csv.writer(out_file)
-            wr.writerow(scoutingData)
+    data = qr_reader.decode(qr_reader.read_camera())
+    if(data):
+        csv_writer.write_to_csv(data)
         break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-camera.release()
-cv2.destroyAllWindows()
+qr_reader.destroy_windows()
+qr_reader.release_camera()
+csvfile.close()
