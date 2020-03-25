16d619298e1e9ed04744524d655ce0ecaaef5330
David Drobner <david@drobner.ca>
2020-03-25 13:42:46 -0400

    Added functions to verify team and get a match score

--- TBA.py
+++ TBA.py
@@ -2,11 +2,27 @@ import tbapy
 import os
 
 class TBA:
-    def __init__(self):
+    def __init__(self, event):
+
         self.API_KEY = os.getenv('TBA_API_KEY')
         self.tba = tbapy.TBA(self.API_KEY)
+
+        self.event = event
 
     def getstatus(self):
         # Using this to test if I set it up correctly
         print(self.API_KEY)
         print(self.tba.status())
+
+    def verify_team(self, match_number, match_type, alliance, team):
+        # Checks if a team is on the specified alliance in a given match
+        match_data = self.tba.match(key=f"{self.event}_{match_type}m{match_number}")
+        for t in match_data.alliances[alliance]['team_keys']:
+            if f"frc{str(team)}" == t:
+                return True
+        return False
+
+
+    def get_match_score(self, match_number, match_type, alliance):
+        match_data = self.tba.match(key=f"{self.event}_{match_type}m{match_number}")
+        return match_data.alliances[alliance]['score']
--- tba_test.py
+++ tba_test.py
@@ -1,4 +1,4 @@
 from TBA import TBA
 
 tba = TBA('2020onosh')
-print(tba.get_match_score(4, "q", "red"))
+print(tba.verify_team(4, "q", "red", 8081))
