32da2c809bf4cea6b8c4c6ba23b659debc121123
David Drobner <david@drobner.ca>
2020-09-04 01:23:17 -0400

    Created skeleton DataHandler

--- DataHandler.py
+++ DataHandler.py
@@ -6,3 +6,11 @@ class DataHandler:
     def __init__(self, dataDirectory="data"):
         if not os.path.exists(dataDirectory):
             os.mkdir(dataDirectory)
+
+    def readData(self, data: str):
+        # Will just turn the data into YAML and return it
+        pass
+
+    def writeData(self):
+        # Writes the YAML to a file
+        pass
