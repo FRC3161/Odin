8cd5fec906069252c2813b5eb95c069a83a0c1b1
David Drobner <david@drobner.ca>
2020-09-15 20:58:28 -0400

    Added method to check if score is numeric

--- Verify.py
+++ Verify.py
@@ -2,5 +2,12 @@ class Verify:
     def __init__(self):
         pass
 
+    def check_if_score_is_numeric(self, score):
+        try:
+            int(score)
+        except ValueError:
+            return False
+        return True
+
     def verify_offline(self):
         pass
