5aeadf8e79829bb4e388311c3915a2bf72f5dfc4
David Drobner <david@drobner.ca>
2020-10-03 16:27:42 -0400

    Added method to check if score is within acceptable bounds

--- Verify.py
+++ Verify.py
@@ -1,3 +1,5 @@
+import Constants
+
 class Verify:
     def __init__(self):
         pass
@@ -7,6 +9,11 @@ class Verify:
         try:
             int(score)
         except ValueError:
+            return False
+        return True
+
+    def check_score_bounds(self, score):
+        if (Constants.SCORE_MIN < score) or (Constants.SCORE_MAX < score):
             return False
         return True
 
